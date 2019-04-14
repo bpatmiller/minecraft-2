91a29dc3547da99276492359bcc38baa4753e2b1
brendan miller <brendanmiller.ca@gmail.com>
2019-04-14 17:34:25 -0500

    added glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <GL/glew.h>
+#include <glm/gtx/io.hpp>
+#include <glm/gtx/string_cast.hpp>
 #include <iostream>
 
-int main(int argc, char *argv[]) { 
-    std::cout << "hello" << std::endl;
-    }
+int main(int argc, char *argv[]) {
+  std::cout << "hello" << std::endl;
+  glm::vec3 v(1.0);
+  std::cout << glm::to_string(v) << std::endl;
+}
