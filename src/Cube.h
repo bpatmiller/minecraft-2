2a29f97365dd4e98ec7e787b12f0d96f3f2950a2
brendan miller <brendanmiller.ca@gmail.com>
2019-04-16 02:01:56 -0500

    reformat, resize

--- src/Cube.h
+++ src/Cube.h
@@ -13,15 +13,13 @@ struct Cube {
                                      {1.0f, 1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}};
 
   std::vector<glm::uvec3> indices = {
-      {0, 2, 1}, {1, 2, 3}, {4, 5, 6}, {5, 7, 6}, {0, 1, 5},
-      {0, 5, 4}, {2, 7, 3}, {2, 6, 7}, {3, 5, 1}, {3, 7, 5},
-      {0, 6, 2}, {0, 4, 6}
-  };
+      {0, 2, 1}, {1, 2, 3}, {4, 5, 6}, {5, 7, 6}, {0, 1, 5}, {0, 5, 4},
+      {2, 7, 3}, {2, 6, 7}, {3, 5, 1}, {3, 7, 5}, {0, 6, 2}, {0, 4, 6}};
 
   std::vector<glm::vec3> offsets = {{0.0f, 0.0f, 0.0f}};
 
   Cube() {
-    // generateTerrain();
+    generateTerrain();
     VAO.vb.bindVertices(vertices);
     VAO.ib.bindVertices(offsets);
     VAO.setLayout({3}, false);
@@ -37,10 +35,10 @@ struct Cube {
   void generateTerrain() {
     offsets.clear();
 
-    int minx = -5;
-    int maxx = 5;
-    int minz = -5;
-    int maxz = 5;
+    int minx = -100;
+    int maxx = 100;
+    int minz = -100;
+    int maxz = 100;
 
     for (int x = minx; x < maxx; x++) {
       for (int z = minz; z < maxz; z++) {
